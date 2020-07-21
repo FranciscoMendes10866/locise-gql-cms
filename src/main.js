0c69120385619c100fb029990e29ad3227b5320a
Francisco Mendes <franciscomendes@Franciscos-MBP.lan>
2020-07-21 17:13:44 +0100

    init commit && connection made && some styling done

--- src/main.js
+++ src/main.js
@@ -1,11 +1,24 @@
 import Vue from 'vue'
+import ApolloClient from 'apollo-boost'
+import VueApollo from 'vue-apollo'
+
 import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
+import './assets/css/tailwind.css'
 
 Vue.config.productionTip = false
 
+const apolloProvider = new VueApollo({
+  defaultClient: new ApolloClient({
+    uri: process.env.VUE_APP_GQL_API
+  })
+})
+
+Vue.use(VueApollo)
+
 new Vue({
   router,
+  apolloProvider,
   render: h => h(App)
 }).$mount('#app')
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,24 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <section class="bg-gray-100">
+    <div class="container mx-auto">
+      <ApolloQuery :query="require('../graphql/GetLinks.gql')">
+        <template v-slot="{ result: { error, data } }">
+          <h1 v-if="error">An error occurred, please try again later.</h1>
+          <div v-if="data" class="flex flex-auto justify-center items-center text-center h-screen">
+            <div class="max-w-md">
+              <a v-for="item in data.locises" :key="item.id" :href="item.url" target="blank">
+                <h2
+                  class="font-medium hover:font-semibold transform hover:scale-110 opacity-50 hover:opacity-100 px-24 py-8 rounded shadow hover:shadow-xl mt-8 bg-white hover:bg-purple-700 text-black hover:text-white"
+                >{{ item.title }}</h2>
+              </a>
+            </div>
+          </div>
+        </template>
+      </ApolloQuery>
+    </div>
+  </section>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+export default {}
 </script>
